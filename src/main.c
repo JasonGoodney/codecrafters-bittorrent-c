191d68bf7e7780496dbeb36fcdfef90c3b6486c6
jason goodney <jason.goodney@gmail.com>
2026-01-25 15:17:07 -0500

    decode lists

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
+#include <assert.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,35 +11,190 @@ is_digit(char c)
     return c >= '0' && c <= '9';
 }
 
+enum bencode_type {
+    BENCODE_BYTE_STRING,
+    BENCODE_INTEGER,
+    BENCODE_LIST,
+    BENCODE_DICTIONARY
+};
+
+typedef union bencode Bencode;
+
+struct bencode_integer {
+    enum bencode_type type;
+    uint32_t padding1;
+    int64_t value;
+    uint64_t padding2;
+    uint64_t padding3;
+};
+
+struct bencode_byte_string {
+    enum bencode_type type;
+    uint32_t padding1;
+    char *value;
+    size_t length;
+    uint64_t padding2;
+};
+
+struct bencode_list {
+    enum bencode_type type;
+    uint32_t padding1;
+    Bencode *items;
+    size_t length;
+    uint64_t padding2;
+};
+
+struct bencode_kvpair {
+    struct bencode_byte_string key;
+    Bencode *value;
+    uint64_t padding;
+};
+
+struct bencode_dictionary {
+    enum bencode_type type;
+    uint32_t padding1;
+    struct bencode_kvpair *kvpairs;
+    size_t length;
+    uint64_t padding2;
+};
+
+union bencode {
+    enum bencode_type type;
+    struct bencode_byte_string byte_string;
+    struct bencode_integer integer;
+    struct bencode_list list;
+    struct bencode_dictionary dictionary;
+};
+
+struct arena {
+    void *base;
+    uint32_t capacity;
+    uint32_t used;
+};
+
+void *
+arena_push(struct arena *arena, size_t size, size_t nitems)
+{
+    assert((size * nitems) + arena->used <= arena->capacity);
+
+    void *ptr = (uint8_t *)arena->base + arena->used;
+    arena->used += (size * nitems);
+    return ptr;
+}
+
+void
+arena_clear(struct arena *arena)
+{
+    arena->used = 0;
+}
+
+struct arena
+arena_init(uint32_t capacity)
+{
+    struct arena arena = {0};
+    arena.capacity = capacity;
+    arena.base = malloc(capacity);
+    return arena;
+}
+
+void
+arena_destroy(struct arena *arena)
+{
+    free(arena->base);
+}
+
 char *
-decode_bencode(const char *bencoded_value)
+decode_bencode(struct arena *arena, Bencode *bencode, char *bencoded_value);
+
+char *
+decode_byte_string(struct arena *arena, struct bencode_byte_string *bencode, char *bencoded_value)
+{
+    int length = atoi(bencoded_value);
+    size_t lengthlength = snprintf(NULL, 0, "%d", length);
+    const char *colon_index = strchr(bencoded_value, ':');
+    if (colon_index != NULL) {
+        const char *start = colon_index + 1;
+        // bencode->value = arena_push(arena, sizeof(*bencode->value), length + 1);
+        bencode->value = malloc(length + 1);
+        memcpy(bencode->value, start, length);
+        bencode->value[length] = '\0';
+        bencode->length = length;
+    }
+    else {
+        fprintf(stderr, "Invalid encoded value: %s\n", bencoded_value);
+        exit(1);
+    }
+    char *next = bencoded_value+length+lengthlength+1;
+    return next;
+}
+
+char *
+decode_integer(struct arena *arena, struct bencode_integer *bencode, char *bencoded_value)
+{
+    char *token = strtok(bencoded_value + 1, "e");
+    int64_t result = strtoll(token, NULL, 10);
+    bencode->value = result;
+
+    char *next = bencoded_value+strlen(token)+2;
+    return next;
+}
+
+char *
+decode_list(struct arena *arena, struct bencode_list *list, char *bencoded_value)
+{
+    list->length = 0;
+    list->items = (Bencode *)arena_push(arena, sizeof(Bencode *), 8);
+    bencoded_value++;
+    while (*bencoded_value != '\0' && *bencoded_value != 'e') {
+
+        bencoded_value = decode_bencode(arena, &list->items[list->length], bencoded_value);
+        list->length++;
+    }
+
+    return bencoded_value+1;
+}
+
+char *
+decode_bencode(struct arena *arena, Bencode *bencode, char *bencoded_value)
 {
     if (is_digit(bencoded_value[0])) {
-        int length = atoi(bencoded_value);
-        const char *colon_index = strchr(bencoded_value, ':');
-        if (colon_index != NULL) {
-            const char *start = colon_index + 1;
-            char *decoded_str = (char *)malloc(length + 1);
-            strncpy(decoded_str, start, length);
-            decoded_str[length] = '\0';
-            return decoded_str;
-        }
-        else {
-            fprintf(stderr, "Invalid encoded value: %s\n", bencoded_value);
-            exit(1);
-        }
+        bencode->type = BENCODE_BYTE_STRING;
+        bencoded_value = decode_byte_string(arena, (struct bencode_byte_string*)bencode, bencoded_value);
     }
     else if ('i' == bencoded_value[0]) {
-        char *token = strtok((char *)(bencoded_value + 1), "e");
-        int length = strlen(token);
-        char *decoded_str = malloc(length + 1);
-        strncpy(decoded_str, token, length);
-        decoded_str[length] = '\0';
-        return decoded_str;
+        bencode->type = BENCODE_INTEGER;
+        bencoded_value = decode_integer(arena, (struct bencode_integer *)bencode, bencoded_value);
+    }
+    else if ('l' == bencoded_value[0]) {
+        bencode->type = BENCODE_LIST;
+        bencoded_value = decode_list(arena, (struct bencode_list *)bencode, bencoded_value);
     }
     else {
         fprintf(stderr, "Only strings are supported at the moment\n");
         exit(1);
+    }
+
+    return bencoded_value;
+}
+
+void
+bencode_print(Bencode *bencode) {
+    if (bencode->type == BENCODE_BYTE_STRING) {
+        printf("\"%s\"", bencode->byte_string.value);
+    }
+    else if (bencode->type == BENCODE_INTEGER) {
+        printf("%lld", bencode->integer.value);
+    }
+    else if (bencode->type == BENCODE_LIST) {
+        char *delim = "";
+        printf("[");
+        for (int i = 0; i < bencode->list.length; i++) {
+            printf("%s", delim);
+            delim = ",";
+            Bencode *item = &bencode->list.items[i];
+            bencode_print(item);
+        }
+        printf("]");
     }
 }
 
@@ -55,25 +212,26 @@ main(int argc, char *argv[])
 
     const char *command = argv[1];
 
+    uint32_t capacity = 1024 * 1024 * 64;
+    struct arena arena = arena_init(capacity);
+
     if (strcmp(command, "decode") == 0) {
         // You can use print statements as follows for debugging, they'll be
         // visible when running tests.
         fprintf(stderr, "Logs from your program will appear here!\n");
 
-        const char *encoded_str = argv[2];
-        char *decoded_str = decode_bencode(encoded_str);
-        if (atoi(decoded_str)) {
-            printf("%s\n", decoded_str);
-        }
-        else {
-            printf("\"%s\"\n", decoded_str);
-        }
-        free(decoded_str);
+        char *encoded_str = argv[2];
+        Bencode *bencode = arena_push(&arena, sizeof(*bencode), 1);
+        decode_bencode(&arena, bencode, encoded_str);
+        bencode_print(bencode);
+        printf("\n");
     }
     else {
         fprintf(stderr, "Unknown command: %s\n", command);
         return 1;
     }
+
+    arena_destroy(&arena);
 
     return 0;
 }
